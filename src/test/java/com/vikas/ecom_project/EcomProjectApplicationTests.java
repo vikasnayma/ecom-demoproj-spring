1b742079e4929eec202f02fdfe3eeeb758f9dbf3
vikasnayma <vikasnaymaofficial@gmail.com>
2025-02-10 19:06:10 +0530

    sync sources

---
+++ src/test/java/com/vikas/ecom_project/EcomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.ecom_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
